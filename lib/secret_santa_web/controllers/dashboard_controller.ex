94364e3d977fc6240cc60bc3a721feb653cbb1b6
Tony Rowan <trowan812@gmail.com>
2022-04-17 14:52:33 +0100

    Show members of your group on dashboard page

--- lib/secret_santa_web/controllers/dashboard_controller.ex
+++ lib/secret_santa_web/controllers/dashboard_controller.ex
@@ -2,10 +2,11 @@ defmodule SecretSantaWeb.DashboardController do
   use SecretSantaWeb, :controller
 
   alias SecretSanta.Groups
+  alias SecretSanta.Repo
 
   def show(conn, _params) do
     user = conn.assigns.current_user
-    group = Groups.get_group_for_user(user)
+    group = Groups.get_group_for_user(user) |> Repo.preload(:users)
 
     conn |> assign(:group, group) |> render("show.html")
   end
