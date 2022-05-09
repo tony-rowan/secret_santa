51005f94be6aac0dc5efb50abe5a2264d4269cc0
Tony Rowan <tony-rowan@cookpad.com>
2022-05-09 14:41:37 +0100

    Fix UserSessionControllerTest

--- test/secret_santa_web/controllers/user_session_controller_test.exs
+++ test/secret_santa_web/controllers/user_session_controller_test.exs
@@ -11,14 +11,14 @@ defmodule SecretSantaWeb.UserSessionControllerTest do
     test "renders log in page", %{conn: conn} do
       conn = get(conn, Routes.user_session_path(conn, :new))
       response = html_response(conn, 200)
-      assert response =~ "<h1>Log in</h1>"
-      assert response =~ "Register</a>"
-      assert response =~ "Forgot your password?</a>"
+      assert response =~ "Log in"
+      assert response =~ "Register"
+      assert response =~ "Forgot your password?"
     end
 
     test "redirects if already logged in", %{conn: conn, user: user} do
       conn = conn |> log_in_user(user) |> get(Routes.user_session_path(conn, :new))
-      assert redirected_to(conn) == "/"
+      assert redirected_to(conn) == Routes.home_path(conn, :show)
     end
   end
 
@@ -30,28 +30,13 @@ defmodule SecretSantaWeb.UserSessionControllerTest do
         })
 
       assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+      assert redirected_to(conn) == Routes.home_path(conn, :show)
+      assert conn.resp_cookies["_secret_santa_web_user_remember_me"]
 
       # Now do a logged in request and assert on the menu
-      conn = get(conn, "/")
+      conn = get(conn, Routes.home_path(conn, :show))
       response = html_response(conn, 200)
-      assert response =~ user.email
-      assert response =~ "Settings</a>"
-      assert response =~ "Log out</a>"
-    end
-
-    test "logs the user in with remember me", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{
-            "email" => user.email,
-            "password" => valid_user_password(),
-            "remember_me" => "true"
-          }
-        })
-
-      assert conn.resp_cookies["_secret_santa_web_user_remember_me"]
-      assert redirected_to(conn) == "/"
+      assert response =~ user.name
     end
 
     test "logs the user in with return to", %{conn: conn, user: user} do
@@ -75,7 +60,7 @@ defmodule SecretSantaWeb.UserSessionControllerTest do
         })
 
       response = html_response(conn, 200)
-      assert response =~ "<h1>Log in</h1>"
+      assert response =~ "Log in"
       assert response =~ "Invalid email or password"
     end
   end
@@ -85,14 +70,12 @@ defmodule SecretSantaWeb.UserSessionControllerTest do
       conn = conn |> log_in_user(user) |> delete(Routes.user_session_path(conn, :delete))
       assert redirected_to(conn) == "/"
       refute get_session(conn, :user_token)
-      assert get_flash(conn, :info) =~ "Logged out successfully"
     end
 
     test "succeeds even if the user is not logged in", %{conn: conn} do
       conn = delete(conn, Routes.user_session_path(conn, :delete))
       assert redirected_to(conn) == "/"
       refute get_session(conn, :user_token)
-      assert get_flash(conn, :info) =~ "Logged out successfully"
     end
   end
 end
