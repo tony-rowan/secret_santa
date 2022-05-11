655e2c62150ca7f5c61de3c4f6012f35968b77fd
Tony Rowan <tony-rowan@cookpad.com>
2022-05-11 15:32:59 +0100

    DRY-up WelcomeTest

--- test/secret_santa_web/features/welcome_test.exs
+++ test/secret_santa_web/features/welcome_test.exs
@@ -1,35 +1,36 @@
 defmodule SecretSantaWeb.WelcomeTest do
   use SecretSanta.FixtureCase
 
+  import SecretSanta.AccountsFixtures
+  import SecretSanta.GroupsFixtures
+
+  @user_attributes %{name: "Niklaus", email: "niklaus@gmail.com", password: "password"}
+
   feature "user without a group logs in", %{session: session} do
-    SecretSanta.AccountsFixtures.user_fixture(
-      %{name: "Niklaus", email: "niklaus@gmail.com", password: "password"}
-    )
+    user_fixture(@user_attributes)
 
     session
     |> visit("/")
     |> click(link("Log In"))
-    |> fill_in(text_field("Email"), with: "niklaus@gmail.com")
-    |> fill_in(text_field("Password"), with: "password")
+    |> fill_in(text_field("Email"), with: @user_attributes[:email])
+    |> fill_in(text_field("Password"), with: @user_attributes[:password])
     |> click(button("Log In"))
-    |> assert_has(link("Niklaus"))
+    |> assert_has(link(@user_attributes[:name]))
     |> assert_has(link("Join a Group"))
     |> assert_has(link("Create a Group"))
   end
 
   feature "user with a group logs in", %{session: session} do
-    user = SecretSanta.AccountsFixtures.user_fixture(
-      %{name: "Niklaus", email: "niklaus@gmail.com", password: "password"}
-    )
-    SecretSanta.GroupsFixtures.group_fixture(%{admin: user, name: "Nice List"})
+    user = user_fixture(@user_attributes)
+    group_fixture(%{admin: user, name: "Nice List"})
 
     session
     |> visit("/")
     |> click(link("Log In"))
-    |> fill_in(text_field("Email"), with: "niklaus@gmail.com")
-    |> fill_in(text_field("Password"), with: "password")
+    |> fill_in(text_field("Email"), with: @user_attributes[:email])
+    |> fill_in(text_field("Password"), with: @user_attributes[:password])
     |> click(button("Log In"))
-    |> assert_has(link("Niklaus"))
+    |> assert_has(link(@user_attributes[:name]))
     |> assert_has_text("You are part of the Nice List secret santa group")
   end
 
@@ -37,10 +38,11 @@ defmodule SecretSantaWeb.WelcomeTest do
     session
     |> visit("/")
     |> click(link("Register"))
-    |> fill_in(text_field("Name"), with: "Niklaus")
-    |> fill_in(text_field("Email"), with: "niklaus@gmail.com")
-    |> fill_in(text_field("Password"), with: "password")
+    |> fill_in(text_field("Name"), with: @user_attributes[:name])
+    |> fill_in(text_field("Email"), with: @user_attributes[:email])
+    |> fill_in(text_field("Password"), with: @user_attributes[:password])
     |> click(button("Register"))
+    |> assert_has(link(@user_attributes[:name]))
     |> assert_has(link("Join a Group"))
     |> assert_has(link("Create a Group"))
   end
