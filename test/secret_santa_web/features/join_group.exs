7a3e1d23ed27d86edbf571ab8ab794d4999af1fe
Tony Rowan <tony-rowan@cookpad.com>
2022-05-12 11:18:45 +0100

    Add browser test for following a join link

--- test/secret_santa_web/features/join_group.exs
+++ test/secret_santa_web/features/join_group.exs
@@ -1,12 +1,17 @@
 defmodule SecretSantaWeb.JoinGroupTest do
   use SecretSanta.FixtureCase
 
+  @user_attrs %{
+    name: "Niklaus",
+    email: "niklaus@gmail.com",
+    password: "password"
+  }
   @group_attrs %{
     name: "Nice List",
     rules: "Be good, or you get coal instead of presents!"
   }
 
-  feature "user joins a group", %{session: session} do
+  feature "user joins a group after logging in", %{session: session} do
     group = SecretSanta.GroupsFixtures.group_fixture(@group_attrs)
 
     session
@@ -15,6 +20,20 @@ defmodule SecretSantaWeb.JoinGroupTest do
     |> click(link("Join a Group"))
     |> fill_in(text_field("Join code"), with: group.join_code)
     |> click(button("Join Group"))
+    |> assert_has_text("You are part of the #{@group_attrs[:name]} secret santa group")
+    |> assert_has(Query.text(@group_attrs[:rules]))
+    |> assert_has(link(@group_attrs[:name]))
+  end
+
+  feature "user joins group as part of registration", %{session: session} do
+    group = SecretSanta.GroupsFixtures.group_fixture(@group_attrs)
+
+    session
+    |> visit("/join/#{group.join_code}")
+    |> fill_in(text_field("Name"), with: @user_attrs[:name])
+    |> fill_in(text_field("Email"), with: @user_attrs[:email])
+    |> fill_in(text_field("Password"), with: @user_attrs[:password])
+    |> click(button("Register"))
     |> assert_has_text("You are part of the #{@group_attrs[:name]} secret santa group")
     |> assert_has(Query.text(@group_attrs[:rules]))
     |> assert_has(link(@group_attrs[:name]))
